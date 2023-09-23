59c14a155650738bc7fdb56c7146b2bc97de58a7
An1MuS1111 <khalidrafi1111@gmail.com>
2023-09-24 02:44:58 +0600

    different file

---
+++ backend/routes/paginatedResults.js
@@ -0,0 +1,41 @@
+function paginatedResults(model) {
+    return (async (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const results = {};
+
+
+        if (endIndex < model.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+
+            }
+        }
+
+
+        if (startIndex < 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        try {
+            results.result = await model.find().limit(limit).skip(startIndex).exec()
+            res.paginatedResults = results
+            console.log(results)
+            next()
+        }
+        catch (err) {
+            res.status(500).json({ message: err.message })
+        }
+
+    })
+}
+
+module.exports = { paginatedResults }
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,50 +1,51 @@
 const router = require('express').Router()
 let Product = require('../models/product.model')
+const { paginatedResults } = require('./paginatedResults')
 
 
 router.route('/').get(paginatedResults(Product), (req, res) => {
     res.json(res.paginatedResults)
 })
 
-function paginatedResults(model) {
-    return (async (req, res, next) => {
-        const page = parseInt(req.query.page)
-        const limit = parseInt(req.query.limit)
+// function paginatedResults(model) {
+//     return (async (req, res, next) => {
+//         const page = parseInt(req.query.page)
+//         const limit = parseInt(req.query.limit)
 
-        const startIndex = (page - 1) * limit
-        const endIndex = page * limit
+//         const startIndex = (page - 1) * limit
+//         const endIndex = page * limit
 
-        const results = {};
-
-
-        if (endIndex < model.length) {
-            results.next = {
-                page: page + 1,
-                limit: limit
-
-            }
-        }
+//         const results = {};
 
 
-        if (startIndex < 0) {
-            results.previous = {
-                page: page - 1,
-                limit: limit
-            }
-        }
+//         if (endIndex < model.length) {
+//             results.next = {
+//                 page: page + 1,
+//                 limit: limit
 
-        try {
-            results.result = await model.find().limit(limit).skip(startIndex).exec()
-            res.paginatedResults = results
-            console.log(results)
-            next()
-        }
-        catch (err) {
-            res.status(500).json({ message: err.message })
-        }
+//             }
+//         }
 
-    })
-}
+
+//         if (startIndex < 0) {
+//             results.previous = {
+//                 page: page - 1,
+//                 limit: limit
+//             }
+//         }
+
+//         try {
+//             results.result = await model.find().limit(limit).skip(startIndex).exec()
+//             res.paginatedResults = results
+//             console.log(results)
+//             next()
+//         }
+//         catch (err) {
+//             res.status(500).json({ message: err.message })
+//         }
+
+//     })
+// }
 
 
 router.route('/add').post((req, res) => {
