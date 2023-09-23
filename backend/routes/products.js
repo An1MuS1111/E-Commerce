d2edf7c66113d001d3c2aa91f3538ecbbe74f7f8
An1MuS1111 <khalidrafi1111@gmail.com>
2023-09-24 02:06:01 +0600

    pagination created

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,11 +2,56 @@ const router = require('express').Router()
 let Product = require('../models/product.model')
 
 
-router.route('/').get((req, res) => {
-    Product.find()
-        .then(products => res.json(products))
-        .catch(err => res.status(400).json('Error: ' + err))
+// router.route('/').get((req, res) => {
+//     Product.find()
+//         .then(products => res.json(products))
+//         .catch(err => res.status(400).json('Error: ' + err))
+// })
+
+router.post('/', paginatedResults(Product), (req, res) => {
+    res.json(res.paginatedResults)
 })
+
+
+function paginatedResults(model) {
+    return (async (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const results = {};
+
+
+        if (endIndex < model.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+
+            }
+        }
+
+
+        if (startIndex < 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        try {
+            results.result = await model.find().limit(limit).skip(startIndex).exec()
+            res.paginatedResults = results
+            console.log(results)
+            next()
+        }
+        catch (err) {
+            res.status(500).json({ message: err.message })
+        }
+
+    })
+}
 
 
 router.route('/add').post((req, res) => {
@@ -26,6 +71,9 @@ router.route('/add').post((req, res) => {
         .then(() => res.json('Product added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 })
+
+
+
 
 
 module.exports = router
