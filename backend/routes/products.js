2021519eac319ad22d189a5d1c8d41b4247418c2
An1MuS1111 <khalidrafi1111@gmail.com>
2023-09-24 02:49:03 +0600

    from another file

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -7,45 +7,6 @@ router.route('/').get(paginatedResults(Product), (req, res) => {
     res.json(res.paginatedResults)
 })
 
-// function paginatedResults(model) {
-//     return (async (req, res, next) => {
-//         const page = parseInt(req.query.page)
-//         const limit = parseInt(req.query.limit)
-
-//         const startIndex = (page - 1) * limit
-//         const endIndex = page * limit
-
-//         const results = {};
-
-
-//         if (endIndex < model.length) {
-//             results.next = {
-//                 page: page + 1,
-//                 limit: limit
-
-//             }
-//         }
-
-
-//         if (startIndex < 0) {
-//             results.previous = {
-//                 page: page - 1,
-//                 limit: limit
-//             }
-//         }
-
-//         try {
-//             results.result = await model.find().limit(limit).skip(startIndex).exec()
-//             res.paginatedResults = results
-//             console.log(results)
-//             next()
-//         }
-//         catch (err) {
-//             res.status(500).json({ message: err.message })
-//         }
-
-//     })
-// }
 
 
 router.route('/add').post((req, res) => {
